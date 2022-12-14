a601b8767de49eb0eb6fa78625f76539f970a72e
Karina <kbrilhante@gmail.com>
2022-12-14 09:39:17 -0300

    Update canvas.js

--- script/canvas.js
+++ script/canvas.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("canvas");
 const width = 98; // 98%
-const height = 96; // 98%
+const height = 100; // 98%
 
 inicialize();
 
@@ -10,12 +10,20 @@ function inicialize() {
 }
 
 function resizeCanvas() {
-    const content = document.getElementsByClassName("contentDiv");
+    const contents = document.getElementsByClassName("contentDiv");
     const container = document.querySelector(".cnvContent");
     let contentHeights = 0;
-    for (let i = 0; i < content.length; i++) {
-        contentHeights += content[i].clientHeight;
+    for (let i = 0; i < contents.length; i++) {
+        const element = contents[i];
+        const computedStyle = window.getComputedStyle(element);
+        contentHeights += element.clientHeight;
+        contentHeights += parseInt(computedStyle.paddingTop, 10);
+        contentHeights += parseInt(computedStyle.paddingBottom, 10);
+        contentHeights += parseInt(computedStyle.marginTop, 10);
+        contentHeights += parseInt(computedStyle.marginBottom, 10);
+        contentHeights += parseInt(computedStyle.borderTopWidth, 10);
+        contentHeights += parseInt(computedStyle.borderBottomWidth, 10);
     }
     canvas.width = container.clientWidth * (width/100);
-    canvas.height = (window.innerHeight - contentHeights) * (height/100);;
+    canvas.height = (window.innerHeight - contentHeights) * (height/100);
 }
